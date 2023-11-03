33ce12d4bd84f11b19ccf83d4464014e83f1538b
livinglab <jessbarbosa2000@gmail.com>
2023-11-03 14:36:13 -0300

    [200~crawler relative to the first instance tjal created

--- src/crawlers/crawler_tjal_2/crawler_tjal_2.controller.ts
+++ src/crawlers/crawler_tjal_2/crawler_tjal_2.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, NotFoundException, Param } from '@nestjs/common';
+import { CrawlerTjal2Service } from './crawler_tjal_2.service';
+
 
 @Controller('crawler-tjal-2')
-export class CrawlerTjal2Controller {}
+export class CrawlerTjal2Controller {
+    constructor(private readonly crawlerTjal2: CrawlerTjal2Service) {}
+
+    @Get(':process')
+    async getProcessDetails(@Param('process') processCod: string): Promise<any> {
+      const processData = await this.crawlerTjal2.getProcessTwo(processCod);
+      if (!processData) {
+        throw new NotFoundException(
+          `Processo com codigo ${processCod} não encontrado`,
+        );
+      }
+      return processData;
+    }
+}
