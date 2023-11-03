ada1cdd4510a01763cafee6d4643804f866353fc
livinglab <jessbarbosa2000@gmail.com>
2023-11-03 14:57:09 -0300

    api folders created

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,11 +10,14 @@ import { CrawlerTjal2Module } from './crawlers/crawler_tjal_2/crawler_tjal_2.mod
 import { CrawlerTjce1Module } from './crawlers/crawler_tjce_1/crawler_tjce_1.module';
 import { CrawlerTjce1Controller } from './crawlers/crawler_tjce_1/crawler_tjce_1.controller';
 import { CrawlerTjce1Service } from './crawlers/crawler_tjce_1/crawler_tjce_1.service';
+import { ApiController } from './api/api.controller';
+import { ApiService } from './api/api.service';
+import { ApiModule } from './api/api.module';
 
 
 @Module({
-  imports: [CrawlerTjal1Module, CrawlerTjal2Module, CrawlerTjce1Module],
-  controllers: [AppController, CrawlerTjal1Controller, CrawlerTjal2Controller, CrawlerTjce1Controller],
-  providers: [AppService, CrawlerTjal1Service, CrawlerTjal2Service, CrawlerTjce1Service],
+  imports: [CrawlerTjal1Module, CrawlerTjal2Module, CrawlerTjce1Module, ApiModule],
+  controllers: [AppController, CrawlerTjal1Controller, CrawlerTjal2Controller, CrawlerTjce1Controller, ApiController],
+  providers: [AppService, CrawlerTjal1Service, CrawlerTjal2Service, CrawlerTjce1Service, ApiService],
 })
 export class AppModule {}
